ec2dfede5701cf9dff4178dadf685cbab69165cd
gottiwoodz <pablogotti954@gmail.com>
2026-08-20 22:23:32 -0400

    fin clase 20-08-2026

--- citas/admin.py
+++ citas/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Cliente, Servicio
+
+@admin.register(Servicio)
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "precio", "duracion_min")
+    search_fields = ("nombre",)
+    ordering = ("duracion_min",)
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "telefono", )
+    search_fields = ("nombre",)
+    ordering = ("nombre",)
